399ab2bfdd5f18c762208e604eb5bddc9cde6df7
vikas <vj4511927@gmail.com>
2022-07-19 17:39:01 +0530

    Assessment-1

---
+++ Great learing Assingment 1/src/departManagement/HrDepartment.java
@@ -0,0 +1,21 @@
+package departManagement;
+
+public class HrDepartment extends SuperDepartment{
+	
+	public String departmentName() {
+		return "Welcome to Hr Department";
+	}
+	
+	public String getTodaysWork() {
+		return "Fill today’s worksheet and mark your attendance";
+	}
+	
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	
+	public String doActivity() {
+		return "team Lunch";
+	}
+	
+}
---
+++ Great learing Assingment 1/src/departManagement/TechDepartment.java
@@ -0,0 +1,23 @@
+package departManagement;
+
+public class TechDepartment extends SuperDepartment{
+
+		public String departmentName() {
+			return "Welcome to Tech Department";
+		}
+		
+		
+		public String getTodaysWork() {
+			return "Complete Coading of Module 1";
+		}
+		
+		public String getWorkDeadline() {
+			return "Complete by COD";
+		}
+		
+		public String getTechStackInformation() {
+			return "core Java";
+		}
+}
+
+
